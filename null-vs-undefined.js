8bc1fa66c997baec2e444dda1771ed639ba009c4
Arif <arif.swfu@outlook.com>
2020-03-04 05:06:26 +0800

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,37 @@
+//                                      GET undefined
+
+
+//1
+let name;
+console.log(name);
+
+
+//2
+function add1(num1, num2) {
+    console.log(num1 + num2);
+}
+const result1 = add1(13, 82);
+console.log(result1);
+
+
+//3
+function add2(num1, num2) {
+    console.log(num1 + num2);
+}
+const result2 = add2(13);
+console.log(result2);
+
+
+//4
+const lover = { name:"smart", phone:454523 };
+console.log(lover.gf);
+
+
+//5
+let fun = undefined;
+console.log(fun);
+
+
+//6
+let ages = [2, 5, 7];
+console.log(ages[9]);
