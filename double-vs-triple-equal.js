c7190b1d6bce124d9c34e787859c1762d9e5636a
Arif <arif.swfu@outlook.com>
2020-03-04 05:23:44 +0800

    double vs triple equal details

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,13 @@
+const first = 2;   //1, 0
+const second = "2";  //true, false
+
+if (first == second) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
+
+
+// == type will not check with value
+// === type wil check with value in the time of compare
